f17c3a3e09a40bf6148e2ed13a23286dafa4e5e0
Gabriel Graciano Dias <86334734+gabrielgraciano@users.noreply.github.com>
2024-11-29 15:59:27 -0300

    Create dtns_brasil.R

---
+++ shiny/dtns_brasil.R
@@ -0,0 +1,23 @@
+dtns_brasil <- HTML(
+  '<div style="font-family: Arial, sans-serif; font-size: 16px; line-height: 1.6; color: #333;">
+  <h2 style="color: #0072B2;">Doenças Tropicais Negligenciadas no Brasil</h2>
+    <p>No contexto nacional, as principais doenças tropicais negligenciadas que se fazem presentes
+    são cromoblastomicose, doença de Chagas, envenenamento por picada de cobra, escabiose, esquistossomose,
+    febre chikungunya, filariose linfática, geo-helmintíases, hanseníase, hidatidose, leishmanioses, micetoma
+    , oncocercose, raiva e tracoma. Dentre essas, algumas encontram-se na lista de notificação compulsória de doenças,
+    agravos e eventos de saúde pública, o que possibilita coleta mais refinada de dados acerca das infecções e surtos.</p>
+
+    <p>No Brasil, os dados relativos à saúde nacional estão contidos no Departamento de Informática do SUS (DATASUS),
+    o qual abarca uma série de sistemas diferentes. Dentre os sistemas disponíveis no Datasus, encontra-se o <a href = "https://datasus.saude.gov.br/acesso-a-informacao/doencas-e-agravos-de-notificacao-de-2007-em-diante-sinan/" target="_blank"
+style="color: #0072B2; text-decoration: none;">Sistema de
+Informação e Agravos de Notificação</a> (SINAN), que fornece acesso a dados dos diagnósticos 
+das doenças e agravos de notificação compulsória. Para a realização do presente projeto, todos os 
+dados relativos a doenças foram obtidos do SINAN, já os dados relativos à população e mapas foram retirados
+do Instituto Brasileiro de Geografia e Estatística (IBGE).</p>
+
+    <p>Neste projeto, portanto, realizou-se um estudo ecológico de séries temporais e
+    elaboração de mapas, ambos abarcando um período igual ou superior a dez anos, para a incidência das
+    seguintes doenças tropicais negligenciadas em território brasileiro: doença de Chagas, esquistossomose,
+    envenenamento por picada de cobra, febre chikungunya, hanseníase, leishmanioses e raiva humana.</p>
+  </div>'
+)
