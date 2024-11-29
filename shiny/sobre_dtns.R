b4e1554586adbb9bf876c7c541907a3da3e9c8c3
Gabriel Graciano Dias <86334734+gabrielgraciano@users.noreply.github.com>
2024-11-29 15:59:06 -0300

    Create sobre_dtns.R

---
+++ shiny/sobre_dtns.R
@@ -0,0 +1,50 @@
+sobre_dtns_texto <- HTML(
+  '<div style="font-family: Arial, sans-serif; font-size: 16px; line-height: 1.6; color: #333;">
+  <h2 style="color: #0072B2;">Doenças Tropicais Negligenciadas</h2>
+  
+  <p>As doenças tropicais negligenciadas (DTNs) são um grupo de doenças ou agravos que
+  compartilham determinadas características entre si, como a prevalência em populações desassistidas,
+  especialmente em países de baixa renda em regiões tropicais do globo terrestre, baixa disseminação em escala global
+  e criação de estigmas e preconceitos diante dos afetados.</p>
+
+  <p>Tais doenças, que demandam elevados custos na área da saúde para seu tratamento, geram
+  pouco interesse por parte da indústria farmacêutica e contam com financiamento insuficiente por
+  parte das agências de fomento à pesquisa. Atualmente, cerca de 1,65 bilhão de pessoas requerem tratamento
+  voltado especificamente a essas condições.</p>
+
+  <p>Nesse cenário, a Organização Mundial da Saúde (OMS) lidera as iniciativas globais para 
+  a diminuição dos impactos dessas doenças ao redor do mundo, ressaltando que o acesso facilitado
+  a dados acerca das DTNs, com fins de pesquisa ou promoção de ações nas populações, é de extrema
+  importância para a agenda global de saúde pública, já que melhora a qualidade de suas intervenções 
+  e refina as decisões políticas a serem tomadas.</p>
+
+  <p>De acordo com a OMS, atualmente, consideram-se doenças tropicais negligenciadas as seguintes enfermidades:</p>
+
+  <ul>
+    <li>Úlcera de Buruli</li>
+    <li>Doença de Chagas</li>
+    <li>Dengue</li>
+    <li>Febre chikungunya</li>
+    <li>Dracunculíase</li>
+    <li>Equinococose</li>
+    <li>Trematodiases transmitidas por alimentos</li>
+    <li>Tripanossomíase humana africana</li>
+    <li>Leishmanioses</li>
+    <li>Hanseníase</li>
+    <li>Filariose linfática</li>
+    <li>Micetoma, cromoblastomicose e outras micoses profundas</li>
+    <li>Noma</li>
+    <li>Oncocercose</li>
+    <li>Raiva humana</li>
+    <li>Escabiose e ectoparasitoses</li>
+    <li>Esquistossomose</li>
+    <li>Helmintíases transmitidas pelo solo</li>
+    <li>Envenenamento por picada de cobra</li>
+    <li>Teníase/cisticercose</li>
+    <li>Tracoma</li>
+    <li>Bouba</li>
+  </ul>
+
+  
+  </div>'
+)
