9cd003e862da5dabc7d1460f27d2b3c766b585f2
Gabriel Graciano Dias <86334734+gabrielgraciano@users.noreply.github.com>
2024-11-29 15:49:43 -0300

    Create sobre_projeto.R

---
+++ shiny/sobre_projeto.R
@@ -0,0 +1,33 @@
+sobre_projeto_texto <- HTML('
+  <div style="font-family: Arial, sans-serif; font-size: 16px; line-height: 1.6; color: #333;">
+    <h2 style="color: #0072B2;">Sobre o Projeto</h2>
+    <p>
+      O presente projeto, intitulado <strong>VigiTrop</strong>, dedica-se ao monitoramento de 
+      determinadas doenças tropicais negligenciadas no Brasil. A aplicação fornece análises 
+      gráficas, mapas, predições e informações acerca de cada doença trabalhada. Atualmente,
+      9 doenças estão registradas no banco de dados da aplicação: dengue, doença de Chagas,
+      esquistossomose, envenenamento por picada de cobra, febre chikungunya, hanseníase,
+      leishmaniose visceral, leishmaniose tegumentar americana e raiva humana.
+    </p>
+    <p>
+      A aplicação foi desenvolvida na linguagem R, utilizando a biblioteca Shiny,
+      e seu banco de dados consiste em um servidor MySQL, ambos armazenados em uma
+      máquina virtual cedida pela Universidade Federal de São Paulo ao Departamento de Medicina
+      Preventiva. Os códigos referentes à criação da aplicação, obtenção e manejo de dados,
+      além de breve tutorial de reprodutibilidade estão disponíveis na página do projeto na
+      plataforma GitHub.
+    </p>
+    <p>
+      O projeto é fruto do Trabalho de Conclusão de Curso de bacharelado em Biomedicina
+      do discente Gabriel Graciano Dias, orientado pela Prof. Dra. Camila Bertini Martins
+      e co-orientado pela Prof. Dra. Thaís Cláudia Roma de Oliveira Konstantyner. Ademais, 
+      contou-se com o auxílio técnico da TAE Dra. Alessandra A. S. Menezes.
+    </p>
+    <p style="margin-top: 20px;">
+      <strong>Contato:</strong>
+      <a href="mailto:gabriel.graciano@unifesp.br" style="text-decoration: none; color: #0072B2;">
+        <span style="font-size: 18px;">📧</span> gabriel.graciano@unifesp.br
+      </a>
+    </p>
+  </div>
+')
